1fdae63dd19b1dc6288232081ae0653e0b649c38
bazingaJojo <bazjo73@gmail.com>
2020-07-28 12:45:10 +0200

    refactor cubemx configuration and add crossworks project

---
+++ stm32/MDK-ARM/photodiode_adc THUMB Debug/STM32_Startup.d
@@ -0,0 +1,3 @@
+photodiode_adc\ THUMB\ Debug/STM32_Startup.o: \
+ C:/Users/Admin/AppData/Local/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v4/packages/targets/STM32/STM32_Startup.s \
+ C:/Users/Admin/AppData/Local/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM/v4/packages/targets/STM32/STM32F401xE.vec
---
+++ stm32/MDK-ARM/photodiode_adc THUMB Debug/thumb_crt0.d
@@ -0,0 +1,2 @@
+photodiode_adc\ THUMB\ Debug/thumb_crt0.o: \
+ C:/Program\ Files/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM\ 4.7/source/thumb_crt0.s
